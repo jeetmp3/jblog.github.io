298ab5524599b42d9fddfda14c7d043c30579195
Jitendra Singh <jeet.mp3@gmail.com>
2017-01-28 17:30:05 +1000

    Deploy script changed

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git add . & git commit -m "Deploying..." & git push origin master
+git add . & git commit -m "Deploying..." & ssh-agent bash -c 'ssh-add /home/jitendra/.ssh/id_jeetmp3_rsa; git push origin master'

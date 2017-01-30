2328f8c3d60551daeb19a9e8264b95247afb7c6b
Jitendra Singh <jeet.mp3@gmail.com>
2017-01-30 14:58:29 +1000

    message

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 dateStr=`date --iso-8601='seconds'`
+read -p "Enter Commit Message: " message
+message=${message:-"Deploying on $dateStr"}
 git add .
-git commit -m "Deploying on $dateStr"
+git commit -m message 
 ssh-agent bash -c 'ssh-add /home/jitendra/.ssh/id_jeetmp3_rsa; git push origin master'
